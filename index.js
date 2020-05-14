9acf8bfaba350f42b73af34fe03726eb279f72ff
AstridSuhartono <astrid_sh@yahoo.com>
2020-05-14 16:58:27 +0930

    create the basic function for adding dept, role, and employee but no validation yet

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function addCase() {
             type: "list",
             name: "addcase",
             message: "Which table would you like to add data into [department], [role], or [employee]?",
-            choices: ["department", "role", "employee"]
+            choices: ["department", "role", "employee", "cancel"]
         })
         .then(function (answer) {
             if (answer.addcase === "department") {
@@ -72,14 +72,88 @@ function addCase() {
                     .then(function (answer) {
                         addDeptQuery(answer.deptname);
                     });
-            };
+            }
+            else if (answer.addcase === "role") {
+                //TODO add function to check if department data is not 0
+                inquirer.prompt(
+                    {
+                        type: "input",
+                        name: "title",
+                        message: "What is the role title?"
+                    },
+                    {
+                        type: "input",
+                        name: "salary",
+                        message: "How much is the salary?"
+                    },
+                    {
+                        type: "input",
+                        name: "department",
+                        message: "which department it belongs to?"
+                    })
+                    .then(function (answer) {
+                        addRoleQuery(answer.title, answer.salary, answer.department);
+                    });
+            }
+            else if (answer.addcase === "employee") {
+                //TODO add function to check if role data is not 0
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "firstname",
+                        message: "What is the employee first name?"
+                    },
+                    {
+                        type: "input",
+                        name: "lastname",
+                        message: "What is the employee last name?"
+                    },
+                    {
+                        type: "input",
+                        name: "role",
+                        message: "what is the employee role? (enter the role id number)"
+                    },
+                    {
+                        type: "input",
+                        name: "manager",
+                        message: "Who is the manager? (enter the manager id number)",
+                        default: null
+                    }])
+                    .then(function (answer) {
+                        addEmployeeQuery(answer.firstname, answer.lastname, answer.role, answer.manager);
+                    });
+            }
+            else {
+                start();
+            }
         });
 }
 
-function addDeptQuery(answer) {
-    connection.query("INSERT INTO department SET ?", { name: answer }, function (err) {
+
+//TODO create function to validate the input from inquirer both string max 30 chars and number (int and decimal)
+
+function addDeptQuery(name) {
+    connection.query("INSERT INTO department SET ?", { name: name }, function (err) {
         if (err) throw err;
-        console.log("New department have been successfully added");
+        console.log(name + " department has been successfully added");
+        start();
+    });
+}
+
+function addRoleQuery(title, salary, department) {
+    let role = { title: title, salary: salary, department: department };
+    connection.query("INSERT INTO role SET ?", role, function (err) {
+        if (err) throw err;
+        console.log(title + "role has been successfuly added");
+        start();
+    });
+}
+
+function addEmployeeQuery(firstName, lastName, role, manager) {
+    let employee = { first_name: firstName, last_name: lastName, role_id: role, manager_id: manager };
+    connection.query("INSERT INTO employee SET ?", employee, function (err) {
+        if (err) throw err;
+        console.log("New employee has been successfuly added");
         start();
     });
 }
